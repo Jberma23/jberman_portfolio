5dbf9460015a2ff7d16a79fbade36f02e67025eb
Jberma23 <Jesse.ben.berman@gmail.com>
2023-11-17 17:09:09 -0700

    adding favicon and changing contact

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,18 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "jberman.vercel.app",
+    template: "%s | jberman.vercel.app",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
-    description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    title: "https://jberman.vercel.app/",
+    description: "Co-founder of unkey.dev and founder of planetfall.io",
+    url: "https://jberman.vercel.app/",
+    siteName: "https://jberman.vercel.app/",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://jberman.vercel.app/og.png",
         width: 1920,
         height: 1080,
       },
@@ -38,11 +37,11 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "Jberma23",
     card: "summary_large_image",
   },
   icons: {
-    shortcut: "/favicon.png",
+    shortcut: "/favicon.icon",
   },
 };
 const inter = Inter({
@@ -66,8 +65,9 @@ export default function RootLayout({
         <Analytics />
       </head>
       <body
-        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
-          }`}
+        className={`bg-black ${
+          process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+        }`}
       >
         {children}
       </body>
